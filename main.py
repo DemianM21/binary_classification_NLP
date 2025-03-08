1f7223a13fed00f105ca7146c7ab83ca8e25d987
DemianM21 <Demianm08@gmail.com>
2025-03-08 01:03:50 -0600

    A1

---
+++ main.py
@@ -0,0 +1,11 @@
+# Importar la función train_and_evaluate_model desde el módulo code_binary_classification_NLB
+from src.code_binary_classification_NLB import train_and_evaluate_model
+
+def main():
+    """
+    Función principal que ejecuta el entrenamiento y evaluación del modelo.
+    """
+    train_and_evaluate_model()  # Llamar a la función para entrenar y evaluar el modelo
+
+if __name__ == "__main__":
+    main()  # Ejecutar la función main() cuando el script se ejecuta directamente
